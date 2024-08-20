5abefd3b23b11a4ee50b7e0705a2e7a884848dc9
Victor Pereira <victor386pereira@gmail.com>
2024-08-20 10:07:07 -0300

    :tada:Desktop Finish

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function menuMobile() {
-    let mobileMenu = document.querySelector('.navbar-mobile');
-    if (mobileMenu.classList.contains('open')) {
-        mobileMenu.classList.remove('open');
-        document.querySelector('.menu-icon').src = "./assets/icons8-cardápio-50.png";
-    } else {
-        mobileMenu.classList.add('open')
-        document.querySelector('.menu-icon').src = "./assets/icons8-excluir-50.png"
-    }
+  let mobileMenu = document.querySelector(".navbar-mobile");
+  if (mobileMenu.classList.contains("open")) {
+    mobileMenu.classList.remove("open");
+    document.querySelector(".menu-icon").src =
+      "./assets/icons8-cardápio-50.png";
+  } else {
+    mobileMenu.classList.add("open");
+    document.querySelector(".menu-icon").src = "./assets/icons8-excluir-50.png";
+  }
 }
